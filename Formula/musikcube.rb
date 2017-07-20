56be94feeae31afcbb8726ec5026f465b5c8cb08
Casey Langen <casey.langen@gmail.com>
2017-07-19 17:22:01 -0700

    initial commit.

---
+++ Formula/musikcube.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Musikcube < Formula
+    homepage 'http://musikcube.com'
+    head 'git://github.com/clangen/musikcube.git'
+    url 'https://github.com/clangen/musikcube/archive/0.20.2.tar.gz'
+    sha256 '049005c06bb1df086de3b2add803235e698cc39598edf3bdb982acb55d1d540b'
+    version '0.20.2'
+
+    depends_on 'cmake' => :build
+    depends_on 'boost'
+    depends_on 'libogg'
+    depends_on 'libvorbis'
+    depends_on 'flac'
+    depends_on 'faad2'
+    depends_on 'libmicrohttpd'
+    depends_on 'lame'
+
+    def install
+        system "cmake -DCMAKE_BUILD_TYPE=Release -DHOMEBREW_PREFIX=#{HOMEBREW_PREFIX} -DCMAKE_INSTALL_PREFIX=#{prefix} ."
+        system "make"
+        system "make install"
+    end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-musikcube
+
+a homebrew formula for musikcube, a terminal-based music player.
+
+to install:
+- `brew tap clangen/musikcube`
+- `brew install musikcube`
+- `musikcube`
