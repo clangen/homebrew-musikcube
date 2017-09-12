4d9664c020c1126e9c4e9c820d1edc2acd9ee0f4
Casey Langen <casey.langen@gmail.com>
2017-09-11 17:14:00 -0700

    Updated release hash.

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -4,7 +4,7 @@ class Musikcube < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
     url 'https://github.com/clangen/musikcube/archive/0.25.2.tar.gz'
-    sha256 '90628413e254e59415f49c77008a6d54b489b272ef65865806bdb2526210ecf3'
+    sha256 '0b01b7cd984cc6a1bbc67fc0c43af64f0e9143a6b8dadd36db52a1fe681f2122'
     version '0.25.2'
 
     depends_on 'cmake' => :build
