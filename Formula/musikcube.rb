18b001bb2d795e6076de69e41b0821b4f685cb5a
equal-l2 <eng.equall2@gmail.com>
2018-09-15 03:10:53 +0900

    Modernize the formula
    - conform to `brew audit`
    - use `std_cmake_args` instead of setting the args manually

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -1,27 +1,22 @@
-require 'formula'
-
 class Musikcube < Formula
-    homepage 'http://musikcube.com'
-    head 'git://github.com/clangen/musikcube.git'
-    url 'https://github.com/clangen/musikcube/archive/0.51.0.tar.gz'
-    sha256 '15d01528283ab20a537fe9339a3d46fae1487172a4e1220dbe9ff3a14ae87631'
-    version '0.51.0'
+  homepage "https://musikcube.com"
+  url "https://github.com/clangen/musikcube/archive/0.51.0.tar.gz"
+  sha256 "15d01528283ab20a537fe9339a3d46fae1487172a4e1220dbe9ff3a14ae87631"
+  head "git://github.com/clangen/musikcube.git"
 
-    depends_on 'cmake' => :build
-    depends_on 'boost'
-    depends_on 'libogg'
-    depends_on 'libvorbis'
-    depends_on 'flac'
-    depends_on 'faad2'
-    depends_on 'libmicrohttpd'
-    depends_on 'lame'
-    depends_on 'libev'
-    depends_on 'openssl'
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "faad2"
+  depends_on "flac"
+  depends_on "lame"
+  depends_on "libev"
+  depends_on "libmicrohttpd"
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "openssl"
 
-    def install
-        system "cmake -DCMAKE_BUILD_TYPE=Release -DHOMEBREW_PREFIX=#{HOMEBREW_PREFIX} -DCMAKE_INSTALL_PREFIX=#{prefix} ."
-        system "make"
-        system "cmake ."
-        system "make install"
-    end
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
 end
