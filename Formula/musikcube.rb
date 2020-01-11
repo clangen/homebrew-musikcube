39bf35dab42633ff2290ce4258762e9254cae297
casey langen <casey.langen@gmail.com>
2020-01-10 19:26:16 -0800

    Updated to 0.80.0

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Musikcube < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
-    url 'https://github.com/clangen/musikcube/archive/0.70.0.tar.gz'
-    sha256 '04161e4c79fdb7bc5b69757e231cf31cb8e2719dfd3c70085e3d8823b36e4ce9'
-    version '0.70.0'
+    url 'https://github.com/clangen/musikcube/archive/0.80.0.tar.gz'
+    sha256 '6e3a8e4221d2d4d7cbccc905eaab5155c8c2d17b46119b021ba4468d2248662b'
+    version '0.80.0'
 
     depends_on 'cmake' => :build
     depends_on 'boost'
