7880deea24ca6f212815ca42a336c8b3d92a4267
casey langen <casey.langen@gmail.com>
2022-12-25 19:24:51 -0800

    More fixes found while trying to get the package accepted as part of homebrew-core

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -2,16 +2,14 @@ require "hardware"
 
 class Musikcube < Formula
   desc "Terminal-based audio engine, library, player and server"
-  homepage "http://musikcube.com"
+  homepage "https://musikcube.com"
   url "https://github.com/clangen/musikcube/archive/0.99.1.tar.gz"
   sha256 "c02c2a7c255929a8d315fe5d586413db2555be4d7fa482336c193011a197cce1"
   license "BSD-3-Clause"
-  head "git://github.com/clangen/musikcube.git"
+  head "https://github.com/clangen/musikcube.git"
 
   depends_on "ccache" => :build
   depends_on "cmake" => :build
-  depends_on "git" => :build
-  depends_on "curl"
   depends_on "ffmpeg"
   depends_on "game-music-emu"
   depends_on "gnutls"
@@ -21,10 +19,10 @@ class Musikcube < Formula
   depends_on "libogg"
   depends_on "libopenmpt"
   depends_on "libvorbis"
-  depends_on "ncurses"
   depends_on "openssl@1.1"
   depends_on "taglib"
-  depends_on "taglib"
+  uses_from_macos "curl"
+  uses_from_macos "ncurses"
 
   def install
     @job_count = [Hardware::CPU.cores - 2, 1].max
