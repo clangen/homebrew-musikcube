b6857ccc59d54fd452e8b84c7a3344969edae762
casey langen <casey.langen@gmail.com>
2023-01-02 13:08:53 -0800

    Update to 0.99.3 and address upstream PR comments.

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -1,15 +1,14 @@
-require "hardware"
-
 class Musikcube < Formula
   desc "Terminal-based audio engine, library, player and server"
   homepage "https://musikcube.com"
-  url "https://github.com/clangen/musikcube/archive/0.99.2.tar.gz"
-  sha256 "782ee8aba01b5e5cf4f5959570d087d7a1208a9309e5ae16074894801a8544bf"
+  url "https://github.com/clangen/musikcube/archive/0.99.3.tar.gz"
+  sha256 "584c8225caad46fde83cdd520ef7d4a6633471de9693651f46d520040e9fb51d"
   license "BSD-3-Clause"
   head "https://github.com/clangen/musikcube.git", branch: "master"
 
   depends_on "ccache" => :build
   depends_on "cmake" => :build
+  depends_on "asio"
   depends_on "curl"
   depends_on "ffmpeg"
   depends_on "game-music-emu"
@@ -25,19 +24,11 @@ class Musikcube < Formula
   depends_on "taglib"
 
   def install
-    @job_count = [Hardware::CPU.cores - 2, 1].max
-    system "git",
-              "clone",
-              "--branch",
-              "asio-1-24-0",
-              "https://github.com/chriskohlhoff/asio/",
-              "src/3rdparty/asio/"
-    system "cmake",
-              "-DCMAKE_BUILD_TYPE=Release",
-              "-DHOMEBREW_PREFIX=#{HOMEBREW_PREFIX}",
-              "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-              "."
-    system "make", "-j#{@job_count}"
+    args = std_cmake_args + %w[
+      -DCMAKE_BUILD_TYPE=Release
+    ]
+    system "cmake", ".", *args
+    system "make"
     system "cmake", "."
     system "make", "install"
   end
